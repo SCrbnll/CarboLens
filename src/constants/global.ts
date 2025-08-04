bc1b71db690bb2c96a7e5bb0d94b95834fc2b7f1
SCrbnll <samuelcarbonell1203@gmail.com>
2025-08-04 20:14:27 +0200

    [ADD] AboutSection

---
+++ src/constants/global.ts
@@ -0,0 +1,18 @@
+// import InstragamIcon from "@/components/icons/InstragamIcon.astro";
+import TelegramIcon from "@/components/icons/TelegramIcon.astro";
+
+const TELEGRAM_URL = "https://t.me/carbolens";
+const INSTAGRAM_URL = "https://instagram.com/carbolens";
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    url: INSTAGRAM_URL,
+    icon: TelegramIcon,
+  },
+  {
+    name: "Telegram",
+    url: TELEGRAM_URL,
+    icon: TelegramIcon,
+  }
+];
