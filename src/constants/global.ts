ed991642173221e5ba6a5ead40b4901cee5c7680
SCrbnll <samuelcarbonell1203@gmail.com>
2025-08-07 16:56:23 +0200

    [ADD] CatchSection & [FIX] IntroAnimation

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,18 +1,19 @@
-// import InstragamIcon from "@/components/icons/InstragamIcon.astro";
+import InstagramIcon from "@/components/icons/InstagramIcon.astro";
 import TelegramIcon from "@/components/icons/TelegramIcon.astro";
 
 const TELEGRAM_URL = "https://t.me/carbolens";
 const INSTAGRAM_URL = "https://instagram.com/carbolens";
 
-export const SOCIALS = [
-  {
-    name: "Instagram",
-    url: INSTAGRAM_URL,
-    icon: TelegramIcon,
-  },
-  {
-    name: "Telegram",
-    url: TELEGRAM_URL,
-    icon: TelegramIcon,
-  }
-];
+export const INSTAGRAM_INFO = {
+  name: "Instagram Carbolens",
+  url: INSTAGRAM_URL,
+  username: "carbolens",
+  icon: InstagramIcon
+}
+
+export const TELEGRAM_INFO = {
+  name: "Telegram Carbolens",
+  url: TELEGRAM_URL,
+  username: "carbolens",
+  icon: TelegramIcon
+}
