9d5479408d7b09b6fdb75450a6be31f3c2a23aaa
SCrbnll <samuelcarbonell1203@gmail.com>
2025-08-13 18:48:38 +0200

    [ADD] Details

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,18 +1,19 @@
 import InstagramIcon from "@/components/icons/InstagramIcon.astro";
 import TelegramIcon from "@/components/icons/TelegramIcon.astro";
 
+export const PORTFOLIO_URL = "https://samuelcg.com";
 const TELEGRAM_URL = "https://t.me/carbolens";
-const INSTAGRAM_URL = "https://instagram.com/carbolens";
+const INSTAGRAM_URL = "https://instagram.com/carbolens_";
 
 export const INSTAGRAM_INFO = {
   name: "Instagram Carbolens",
   url: INSTAGRAM_URL,
-  username: "carbolens",
+  username: "carbolens_",
   icon: InstagramIcon
 }
 
 export const TELEGRAM_INFO = {
-  name: "Telegram Carbolens",
+  name: "Canal de Telegram",
   url: TELEGRAM_URL,
   username: "carbolens",
   icon: TelegramIcon
